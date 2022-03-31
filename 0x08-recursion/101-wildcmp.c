9722187c80fd48462e72e5e6c434988bf134d551
Nii Odoi <nii.odoi35@gmail.com>
2022-03-30 21:57:17 -0700

    back to where it all begun

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -28,13 +28,13 @@ int wildcmp(char *s1, char *s2)
 	{
 		s2 = move_char(s2);
 		if (*s2 == '\0')
-			return (0);
+			return (1);
 		if (*s1 == *s2)
 			i += wildcmp(s1 + 1, s2 + 1);
 		i += check_path(s1 + 1, s2);
 		return (!!i);
 	}
-	return (1);
+	return (0);
 }
 
 /**

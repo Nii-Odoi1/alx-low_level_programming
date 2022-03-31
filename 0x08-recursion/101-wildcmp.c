badee1fc3827ee5ad20a836785b7375a527a18c8
Nii Odoi <nii.odoi35@gmail.com>
2022-03-30 21:53:47 -0700

    final update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,19 +22,19 @@ int wildcmp(char *s1, char *s2)
 			return (1);
 	return (wildcmp(s1 + 1, s2 + 1));
 	}
-	if (*s1 == '\0' || *s2 == '*')
+	if (*s1 == '\0' || *s2 == '\0')
 		return (0);
 	if (*s2 == '*')
 	{
 		s2 = move_char(s2);
 		if (*s2 == '\0')
-			return (1);
+			return (0);
 		if (*s1 == *s2)
 			i += wildcmp(s1 + 1, s2 + 1);
 		i += check_path(s1 + 1, s2);
 		return (!!i);
 	}
-	return (0);
+	return (1);
 }
 
 /**
